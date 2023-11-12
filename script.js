0aeb4e0f5ce3b8b5534d3ecfbef036d339186cbc
dodosebn <orjidominion32@gmail.com>
2023-11-12 17:36:45 +0400

    Added modifications and new files

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-let isSignedUp = false;
+var generror = document.getElementById('generror');
+var errorgen = document.getElementById('errorgen');
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+var icon;
+
+if (hourNow < 12){
+greeting = "Welcome ";
+icon = "coffee";
+}
+else if (hourNow < 17){
+greeting = 'Welcome ';
+icon = "sun-o";
+}
+else if (hourNow < 24){
+greeting = "Welcome "
+icon = "moon-o";
+}
+else {
+greeting = "Welcome";
+}
+
+
 
 function toggleForms() {
-  loginContainer.style.display = loginContainer.style.display === "none" ? "block" : "none";
-  signupContainer.style.display = signupContainer.style.display === "none" ? "block" : "none";
+loginContainer.style.display = loginContainer.style.display === "none" ? "block" : "none";
+signupContainer.style.display = signupContainer.style.display === "none" ? "block" : "none";
 }
 
 let errorName = document.getElementById('error-Name');
@@ -13,144 +37,185 @@ let errorcpassword = document.getElementById('error-cpassword');
 let displayLoginError = document.getElementById('displayLoginError');
 
 function validateFullName() {
-  var fullName = document.getElementById('fullName');
-  if (fullName.value === "") {
-    errorName.innerHTML = "Name is required";
-    return false;
-  }
-  const fullNameRegex = /^[a-zA-Z\s]+$/;
-  if (!fullNameRegex.test(fullName.value)) {
-    errorName.innerHTML = "Full name contains invalid characters";
-    return false;
-  } else {
-    errorName.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-  }
+var fullName = document.getElementById('fullName');
+if (fullName.value === "") {
+errorName.innerHTML = "Name is required";
+return false;
+}
+const fullNameRegex = /^[a-zA-Z\s]+$/;
+if (!fullNameRegex.test(fullName.value)) {
+errorName.innerHTML = "Full name contains invalid characters";
+return false;
+} else {
+errorName.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+}
 
-  return true;
+return true;
 }
 
 function validateUserName() {
-  var userName = document.getElementById('userName');
-  if (userName.value === "") {
-    userError.innerHTML = "Username is required";
-    return false;
-  } else {
-    userError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-  }
+var userName = document.getElementById('userName');
+if (userName.value === "") {
+userError.innerHTML = "Username is required";
+return false;
+} else {
+userError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+}
 
-  return true;
+return true;
 }
 
 function validateEmail() {
-  var email = document.getElementById('email');
-  if (email.value === "") {
-    errorEmail.innerHTML = "Email is required";
-    return false;
-  }
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email.value)) {
-    errorEmail.innerHTML = "Please enter a valid email address.";
-    return false;
-  } else {
-    errorEmail.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-  }
+var email = document.getElementById('email');
+if (email.value === "") {
+errorEmail.innerHTML = "Email is required";
+return false;
+}
+const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+if (!emailRegex.test(email.value)) {
+errorEmail.innerHTML = "Please enter a valid email address.";
+return false;
+} else {
+errorEmail.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+}
 
-  return true;
+return true;
 }
 
 function validatePassword() {
-  var password = document.getElementById('password');
-  if (password.value === "") {
-    errorPassword.innerHTML = "Password is required";
-    return false;
-  }
-  
-  if (password.value.length < 6) {
-    errorPassword.innerHTML = "Password is too short";
-    return false;
-  } else {
-    errorPassword.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-  }
+var password = document.getElementById('password');
+if (password.value === "") {
+errorPassword.innerHTML = "Password is required";
+return false;
+}
 
-  return true;
+if (password.value.length < 6) {
+errorPassword.innerHTML = "Password is too short";
+return false;
+} else {
+errorPassword.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+}
+
+return true;
 }
 
 function validateCpassword() {
-  var cPassword = document.getElementById('cPassword');
-  if (cPassword.value === "") {
-    errorcpassword.innerHTML = "Re-enter Password";
-    return false;
-  }
+var cPassword = document.getElementById('cPassword');
+if (cPassword.value === "") {
+errorcpassword.innerHTML = "Re-enter Password";
+return false;
+}
+
+var password = document.getElementById('password');
+if (password.value !== cPassword.value) {
+errorcpassword.innerHTML = "Passwords don't match";
+return false;
+} else {
+errorcpassword.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+}
+
+return true;
+}
+const Logemail = document.getElementById("Logemail");
+    const LogPassword = document.getElementById("LogPassword");
+const fullName = document.getElementById("fullName");
+const userName = document.getElementById("userName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const signupForm = document.getElementById('signupForm');
+const loginForm = document.getElementById('loginForm');
+
+let user = JSON.parse(localStorage.getItem("form"))  || []
+
+document.getElementById("signupForm").addEventListener("submit", function(e) {
+e.preventDefault();
+
+const isFullNameValid = validateFullName();
+const isUserNameValid = validateUserName();
+const isEmailValid = validateEmail();
+const isPasswordValid = validatePassword();
+const isCpasswordValid = validateCpassword();
+
+// If any input is invalid, prevent form submission
+if (
+!isFullNameValid ||
+!isUserNameValid ||
+!isEmailValid ||
+!isPasswordValid ||
+!isCpasswordValid
+) {
+return false;
+}
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const userName = document.getElementById('userName').value;
+
+  const storedUser = localStorage.getItem('user');
+
+  // // Check if email is already registered
+  // if (storedUser) {
+  //   const existingUser = JSON.parse(storedUser);
+  //   if (email === existingUser.email) {
+  //     alert('This email is already registered. Please use a different email.');
+  //     return;
+  //   }
+  // }
+  // Creae new user object
+  const newUser = {
+    userName: userName,
+    email: email,
+    password: password
+  };
+
+  // Store user data in local storage
+  localStorage.setItem('user', JSON.stringify(newUser));
   
-  var password = document.getElementById('password');
-  if (password.value !== cPassword.value) {
-    errorcpassword.innerHTML = "Passwords don't match";
-    return false;
-  } else {
-    errorcpassword.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+ 
+  loginPage();
+  return true;
+});
+
+loginForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // Retrieve form values
+  const loginEmail = document.getElementById('Logemail').value;
+  const loginPassword = document.getElementById('LogPassword').value;
+
+  // Retrieve user data from local storage
+  const storedUser = localStorage.getItem('user');
+  if (!storedUser) {
+    errorgen.textContent = 'Invalid email or password. Please try again.';
+    return;
   }
 
-  return true;
-}
-
-function trickyVal() {
-  const isFullNameValid = validateFullName();
-  const isUserNameValid = validateUserName();
-  const isEmailValid = validateEmail();
-  const isPasswordValid = validatePassword();
-  const isCpasswordValid = validateCpassword();
-
-  // If any input is invalid, prevent form submission
-  if (
-    !isFullNameValid ||
-    !isUserNameValid ||
-    !isEmailValid ||
-    !isPasswordValid ||
-    !isCpasswordValid
-  ) {
-    return false;
-  }
-
-  return true;
-}
-
-let signupButton = document.getElementById('signUp');
-signupButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (trickyVal()) {
+  // Parse user data from JSON string
+  const user = JSON.parse(storedUser);
+  if (loginEmail === user.email && loginPassword === user.password) {
     displayGame();
+    document.getElementById("col-1").innerHTML = "<h3>" + greeting + " </h1>" + "<h3>" + user.userName + "</h1>" ;
+    document.getElementById("icon").innerHTML = ('<i class="fa fa-' + icon + '" aria-hidden="true"></i>');
+
+    loginContainer.style.display = "none";
+  } else {
+    errorgen.textContent = 'Invalid email or password. Please try again';
   }
 });
 
+ 
 function displayGame() {
-  var gameContainer = document.getElementById('container');
-  var signupContainer = document.getElementById('signupContainer');
+var gameContainer = document.getElementById('container');
+var signupContainer = document.getElementById('signupContainer');
+
+gameContainer.style.display = "block";
+signupContainer.style.display = "none";
+}
+function loginPage(){
+    var loginContainer = document.getElementById('loginContainer');
+    loginContainer.style.display = "block";
+    signupContainer.style.display = "none";
+  }
   
-  gameContainer.style.display = "block";
-  signupContainer.style.display = "none";
-}
-
-function validateLoginForm() {
-  const usernameInput = document.getElementById('Lusername');
-  const passwordInput = document.getElementById('LPassword');
-  const errorMessage = document.getElementById('error.message');
-
-  errorMessage.style.display = 'none';
-  errorMessage.innerHTML = '';
-
-  if (usernameInput.value.trim() === '') {
-    errorMessage.style.display = 'block';
-    errorMessage.innerHTML = 'Username field is required.';
-    return false;
-  }
-  if (passwordInput.value === '') {
-    errorMessage.style.display = 'block';
-    errorMessage.innerHTML = 'Password field is required.';
-    return false;
-  }
-
-  return true;
-}
 
 const emojis = ["🐱‍🚀", "🐱‍🚀", "🎂", "🎂", "💖", "💖", "🎶", "🎶"];
 const shuffledEmojis = emojis.sort(() => (Math.random() > 0.5 ? 1 : -1));
@@ -199,140 +264,4 @@ for (let i = 0; i < shuffledEmojis.length; i++) {
 
 document.querySelector(".game").appendChild(box);
 }
-
-const errorMessage = document.getElementById('error.message');
-
-const BASE_URL = "https://memory-game-x3fx.onrender.com/api/v1"; 
-
-const loggedInUserData = {};
-
-function handleSignup() {
-  const isFormValid = trickyVal();
-  
-  if (!isFormValid) {
-    errorMessage.innerHTML = "Form validation failed.";
-    return false;
-  }
-    const userData = {
-      username: fullName.value,
-      email: email.value,
-      password: password.value,
-    };
-  
-    fetch(`${BASE_URL}/users`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Signup failed');
-        }
-      })
-      .then(data => {
-        loggedInUserData.token = data.token;
-        return fetchUserData();
-      })
-      .then(userData => {
-        loggedInUserData.userData = userData;
-        displayGamePage();
-      })
-      .catch(error => {
-        errorMessage.innerHTML = "you Already have an account";
-      });
-    
-
-  isSignedUp = true;
-    signupButton.disabled = true;
-
-    return true;
-  
-}
-
-function handleLogin() {
-
-  if (isSignedUp = true) {
-    const credentials = {
-      username: fullName.value,
-      password: LPassword.value,
-    };
-  
-    fetch(`${BASE_URL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    })
-      .then(response => {
-        if (response.ok) {
-          
-          return response.json();
-        } else {
-          throw new Error('Login failed');
-        }
-      })
-      .then(data => {
-        loggedInUserData.token = data.token;
-      
-        return fetchUserData();
-      })
-      .then(userData => {
-        loggedInUserData.userData = userData;
-        displayGamePage();
-      })
-      .catch(error => {
-        errorMessage.innerHTML = "Signup before you logIN in";
-      });
-  
-    }
-  //  else {
-  //   document.getElementById('error.message').innerHTML = "You have to sign up first.";
-  // }
-
-  if (!validateLoginForm()) {
-    errorMessage.innerHTML = isFormValid = false;
-    return;
-  }
-  return true;
-  
-}
-
-async function fetchUserData() {
-  const response = await fetch(`${BASE_URL}/userdata`, {
-    headers: {
-      'Authorization': `Bearer ${loggedInUserData.token}`,
-    },
-  });
-  if (response.ok) {
-    return response.json();
-  } else {
-    throw new Error('Failed to fetch user data');
-  }
-}
-
-function displayGamePage() {
-  var welcomeMessage = document.getElementById('welcomeMessage');
-  welcomeMessage.innerHTML = `Welcome back,  const {usernameInput.value}, to the memory game!`;
-  var gameContainer = document.getElementById('container');
-  var loginContainer = document.getElementById('loginContainer');
-  
-  gameContainer.style.display = "block";
-  loginContainer.style.display = "none";
-}
-let loginButton = document.getElementById('login');
-loginButton.addEventListener("click", (event) => {
-event.preventDefault();
-if(validateLoginForm()){
-  displayGamePage();
-  console.log(welcomeMessage);
-}
-});
-
-
-
 
